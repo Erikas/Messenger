967f64c0c336b95820bcd62bc24993dcf624e9e5
KristupasV <126498620+KriVait@users.noreply.github.com>
2024-06-11 00:55:26 +0300

    Added some interfaces for attachment services

---
+++ KV/Messenger/Messenger.Core/Services/IAttachmentService.cs
@@ -0,0 +1,10 @@
+﻿using Messenger.Core.Models;
+
+namespace Messenger.Core.Services
+{
+    public interface IAttachmentService
+    {
+        Task<int> Create(INewAttachmentModel model);
+        IQueryable<IAttachmentDtoModel> GetDtoQuery();
+    }
+}
